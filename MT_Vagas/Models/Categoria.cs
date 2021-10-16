f38dc7b491f76265080ffc0d6d744af4bb961fbb
unknown <hmoliterno@hotmail.com>
2021-10-16 17:01:49 -0300

    projeto teste

---
+++ MT_Vagas/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MT_Vagas.Models
+{
+    public class Categoria
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ MT_Vagas/Models/Emprego.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+
+namespace MT_Vagas.Models
+{
+    public class Emprego
+    {
+        public string NomeCategoria { get; set; }
+
+        public Emprego()
+        {
+
+        }
+
+        public Emprego(Categoria categoria)
+        {
+            NomeCategoria = categoria.Nome;
+        }
+    }
+}
